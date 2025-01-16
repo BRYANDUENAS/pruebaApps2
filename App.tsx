974fdb913b743a54eb98c3d96f307233d2b72383
BRYANDUENAS <bryan_duenas96@outlook.com>
2025-01-15 19:42:33 -0600

    cambio app

--- App.tsx
+++ App.tsx
@@ -4,12 +4,8 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'; // Para Bottom Tabs
 import WelcomeScreen from './src/screens/WelcomeScreen';
 import ProductosScreen from './src/screens/ProductosScreen';
-import AddProductsScreen from './src/screens/AddProductsScreen';
-import ApiScreen from './src/screens/ApiScreen';
 import LoginScreen from './src/screens/LoginScreen';
 import RegisterScreen from './src/screens/RegisterScreen';
-import OperacionesScreen from './src/screens/OperacionesScreen';
-import EstudiantesScreen from './src/screens/EstudiantesScreen';
 import PlayScreen from './src/screens/PlayScreen';
 import PerfilScreen from './src/screens/PerfilScreen';
 
@@ -21,8 +17,6 @@ function BottomTabNavigator() {
   return (
     <BottomTabs.Navigator>
       <BottomTabs.Screen name="Productos" component={ProductosScreen} />
-      <BottomTabs.Screen name="AgregarProductos" component={AddProductsScreen} />
-      <BottomTabs.Screen name="ListaApi" component={ApiScreen} />
     </BottomTabs.Navigator>
   );
 }
@@ -34,9 +28,7 @@ export default function App() {
       <Stack.Navigator initialRouteName="WelcomeScreen">
       <Stack.Screen name="WelcomeScreen" component={WelcomeScreen} options={{ headerShown: false }} />
       <Stack.Screen name="LoginScreen" component={LoginScreen} />
-      <Stack.Screen name="RegisterScreen" component={RegisterScreen} />
-      <Stack.Screen name="OperacionesScreen" component={OperacionesScreen} />      
-      <Stack.Screen name="EstudiantesScreen" component={EstudiantesScreen} />   
+      <Stack.Screen name="RegisterScreen" component={RegisterScreen} />  
       <Stack.Screen name="PlayScreen" component={PlayScreen} />  
       <Stack.Screen name="PerfilScreen" component={PerfilScreen} />
 
